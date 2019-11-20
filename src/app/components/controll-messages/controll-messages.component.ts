f02ed090145ef019c5ef020fa804ba34c635db0d
thanhson <kimsonbui303@gmail.com>
2019-11-20 10:27:51 +0700

    update header

--- src/app/components/controll-messages/controll-messages.component.ts
+++ src/app/components/controll-messages/controll-messages.component.ts
@@ -14,7 +14,6 @@ export class ControllMessagesComponent {
   constructor() { }
 
   get errorMessage() {
-    console.log(this.control.errors);
     for (const propertyName in this.control.errors) {
       if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
         return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  navsTab = [
+    { path: '/', active: true, value: 'HOME' },
+    { path: '/resume', value: 'RESUME' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // console.log(window.location.pathname);
+    console.log(document.cookie);
   }
 
+  changeTab(path: string) {
+    this.navsTab = this.navsTab.map(tab => {
+      tab.path === path ? (tab.active = true) : (tab.active = false);
+      return tab;
+    });
+  }
 }
